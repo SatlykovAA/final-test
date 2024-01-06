a44c4e2d910f2d910af708bfdafa460dc520aa1b
AzatSatlykov <azatsatlykov@yahoo.com>
2024-01-06 17:07:53 +0500

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string[] inputArray = Console.ReadLine().Split(',');
+        string[] outputArray = new string[inputArray.Length];
+        int outputIndex = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                outputArray[outputIndex] = inputArray[i];
+                outputIndex++;
+            }
+        }
+        Array.Resize(ref outputArray, outputIndex);
+        Console.WriteLine("Новый массив:");
+        Console.WriteLine(string.Join(", ", outputArray));
+    }
+}

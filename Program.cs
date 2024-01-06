1102e6ba502d08840cffc75f1ec1affa8bf48ec4
AzatSatlykov <azatsatlykov@yahoo.com>
2024-01-06 17:18:16 +0500

    3

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ class Program
     {
         Console.WriteLine("Введите элементы массива через запятую:");
         string[] inputArray = Console.ReadLine().Split(',');
+
+        if (inputArray.Length == 1 && string.IsNullOrEmpty(inputArray[0].Trim()))
+        {
+            Console.WriteLine("Введен пустой массив.");
+            return;
+        }
+
         string[] outputArray = new string[inputArray.Length];
         int outputIndex = 0;
+
         for (int i = 0; i < inputArray.Length; i++)
         {
-            if (inputArray[i].Length <= 3)
+            if (inputArray[i].Trim().Length <= 3)
             {
-                outputArray[outputIndex] = inputArray[i];
+                outputArray[outputIndex] = inputArray[i].Trim();
                 outputIndex++;
             }
         }
+
         Array.Resize(ref outputArray, outputIndex);
+
         Console.WriteLine("Новый массив:");
         Console.WriteLine(string.Join(", ", outputArray));
     }
